3aa6bab0e16ea8d28fb7577945de2682a3ea4000
Marco Eidinger <eidingermarco@gmail.com>
2023-08-03 06:15:28 -0700

    package created and using DummySwiftPackage as dependency

---
+++ Tests/swift-package-monitored-by-dependabotTests/swift_package_monitored_by_dependabotTests.swift
@@ -0,0 +1,12 @@
+import XCTest
+@testable import swift_package_monitored_by_dependabot
+
+final class swift_package_monitored_by_dependabotTests: XCTestCase {
+    func testExample() throws {
+        // XCTest Documenation
+        // https://developer.apple.com/documentation/xctest
+
+        // Defining Test Cases and Test Methods
+        // https://developer.apple.com/documentation/xctest/defining_test_cases_and_test_methods
+    }
+}
